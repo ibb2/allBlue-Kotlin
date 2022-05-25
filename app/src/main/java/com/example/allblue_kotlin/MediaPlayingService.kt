66aba963581190f9bb0159e2ca548c5092f99cca
ibb2 <70052141+ibb2@users.noreply.github.com>
2022-05-25 12:46:34 +1000

    Thread is now looping every 5 seconds checking music status. Thread issue blocking app start resolved.

--- app/src/main/java/com/example/allblue_kotlin/MediaPlayingService.kt
+++ app/src/main/java/com/example/allblue_kotlin/MediaPlayingService.kt
@@ -2,17 +2,14 @@ package com.example.allblue_kotlin
 
 import android.R
 import android.app.*
-import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
 import android.os.Build
 import android.os.IBinder
-import android.provider.MediaStore
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-
-
+import kotlin.concurrent.thread
 
 
 class MediaPlayingService: Service() {
@@ -20,7 +17,7 @@ class MediaPlayingService: Service() {
     private val ONGOING_NOTIFICATION_ID = 1
     private val CHANNEL_DEFAULT_IMPORTANCE = "Media Playing Service"
     private val TAG = "Media Playing Service"
-
+    var audioPlayingStatus: Boolean = false
 
     override fun onBind(p0: Intent?): IBinder? {
         return  null
@@ -32,7 +29,7 @@ class MediaPlayingService: Service() {
         // TODO Detect music playing
 
         val AudioManager: AudioManager = getSystemService(AudioManager::class.java)
-        val AudioManagerActive: Boolean? = AudioManager.isMusicActive
+        val AudioManagerActive: Boolean = AudioManager.isMusicActive
 
         val pendingIntent: PendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
             PendingIntent.getActivity(this, 0, notificationIntent,
@@ -47,16 +44,36 @@ class MediaPlayingService: Service() {
             .setTicker("blank")
             .build()
 
+        thread(true) {
+            while (true) {
+                audioPlayingStatus = false
+
+                if (AudioManagerActive == true) {
+                    audioPlayingStatus = true
+
+                    Intent().also { intent ->
+                        intent.setAction("com.example.allblue_kotlin.MUSIC_ACTIVE_STATUS_CHANGED")
+                        intent.putExtra("data", audioPlayingStatus)
+                        sendBroadcast(intent)
+                    }
+                }
+                Log.i(TAG, "New Initialised thread")
+
+                Thread.sleep(5000)
+            }
+        }
+
         // Start Foreground Service
         startForeground(ONGOING_NOTIFICATION_ID, notification)
 
-        while (!AudioManagerActive!!) {
-
-        }
-
         Log.i(TAG, "Foreground Service running")
-        Log.i(TAG, "Is Music Active: $AudioManagerActive")
+        Log.i(TAG, "Is Music Active: $audioPlayingStatus")
 
         return START_REDELIVER_INTENT
     }
 }
+
+
+
+
+
