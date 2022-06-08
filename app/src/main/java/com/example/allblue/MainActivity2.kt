3bae522213f05da451320594a58a1aa1d9dbda9d
ibb2 <70052141+ibb2@users.noreply.github.com>
2022-06-08 23:34:16 +1000

    Deleted unused imports. Opted into experimental material 3 api's

--- app/src/main/java/com/example/allblue/MainActivity2.kt
+++ app/src/main/java/com/example/allblue/MainActivity2.kt
@@ -7,9 +7,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,8 +14,6 @@ import com.example.compose.Material3AppTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 
 class MainActivity2 : ComponentActivity() {
@@ -26,12 +21,12 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Main() {
-                Greeting(name = "Hello World")
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Main(content: @Composable () -> Unit) {
     Material3AppTheme() {
@@ -66,28 +61,9 @@ fun Main(content: @Composable () -> Unit) {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun CenterAlignedTopAppBar(
-    title: Unit,
-    modifier: Modifier = Modifier,
-    navigationIcon: @Composable () -> Unit = {},
-    actions: @Composable @ExtensionFunctionType RowScope.() -> Unit = {},
-    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
-    scrollBehavior: TopAppBarScrollBehavior? = null
-): Unit {
-    
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Main {
-        Greeting(name = "World")
     }
 }
