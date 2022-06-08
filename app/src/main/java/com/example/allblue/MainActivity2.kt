4d5ba1ca8dd31a270e066d8a0aad3f0e35cb32f6
ibb2 <70052141+ibb2@users.noreply.github.com>
2022-06-08 23:32:24 +1000

    TopAppBar added, Open menu icons added. Dynamic icons for open and closed menus.

--- app/src/main/java/com/example/allblue/MainActivity2.kt
+++ app/src/main/java/com/example/allblue/MainActivity2.kt
@@ -7,22 +7,60 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose.Material3AppTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Material3AppTheme() {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            Main() {
+                Greeting(name = "Hello World")
+            }
+        }
+    }
+}
+
+@Composable
+fun Main(content: @Composable () -> Unit) {
+    Material3AppTheme() {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background) {
+            content()
+            Scaffold(
+                topBar = {
+                   CenterAlignedTopAppBar(
+                       title = { Text(text = "allBlue")},
+                       navigationIcon = {
+                           var menuStatus by remember {
+                               mutableStateOf(false)
+                           }
+
+                           IconButton(onClick = { menuStatus = !menuStatus }) {
+                               if (menuStatus) {
+                                   Icon(painter = painterResource(id = R.drawable.round_menu_open_24), contentDescription ="General settings opened")
+                               } else {
+                                   Icon(painter = painterResource(id = R.drawable.round_menu_24), contentDescription = "General settings")
+                               }
+                           }
+                       }
+                   )
                 }
+
+                ) {
+
             }
         }
     }
@@ -35,7 +73,7 @@ fun Greeting(name: String) {
 
 @Composable
 fun CenterAlignedTopAppBar(
-    title: String,
+    title: Unit,
     modifier: Modifier = Modifier,
     navigationIcon: @Composable () -> Unit = {},
     actions: @Composable @ExtensionFunctionType RowScope.() -> Unit = {},
@@ -49,10 +87,7 @@ fun CenterAlignedTopAppBar(
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Material3AppTheme {
-        Scaffold {
-          CenterAlignedTopAppBar(title ="allBlue")
-        }
-        Greeting("Android")
+    Main {
+        Greeting(name = "World")
     }
 }
