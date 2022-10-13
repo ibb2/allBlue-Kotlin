6fafbaeb86b7f41a4b4d039f3fae7b9f5c9b9cc5
ibb2 <70052141+ibb2@users.noreply.github.com>
2022-10-13 21:27:39 +1100

    Adapty.io installed

--- app/src/main/java/com/subsolis/allblue/AllBlueApplication.kt
+++ app/src/main/java/com/subsolis/allblue/AllBlueApplication.kt
@@ -1,6 +1,7 @@
 package com.subsolis.allblue
 
 import android.app.Application
+import com.adapty.Adapty
 import com.google.android.material.color.DynamicColors
 import com.qonversion.android.sdk.Qonversion
 import dagger.hilt.android.HiltAndroidApp
@@ -14,5 +15,6 @@ class AllBlueApplication: Application() {
             DynamicColors.applyToActivitiesIfAvailable(this)
 //            Qonversion.setDebugMode()
             Qonversion.launch(this, getString(R.string.qonversion_product_key), false)
+            Adapty.activate(applicationContext,"public_live_4Lynd5sb.CfxPkeyLHhmw4pDq7251")
     }
 }
